d5f2b4f1917f68ed84fdf9bba66846332a75e7c1
camposyuri <yurinapoleao@gmail.com>
2020-10-15 13:55:43 -0300

    [ADD] - Create TodoForm and TodoList

---
+++ react-todo-app/src/components/TodoForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const TodoForm = (props) => {
+  const [input, setInput] = useState("");
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+
+    setInput("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="todo-form">
+      <input
+        type="text"
+        placeholder="Add a todo"
+        value={input}
+        name="text"
+        className="todo-input"
+        onChange={handleChange}
+      />
+      <button className="todo-button">Add todo</button>
+    </form>
+  );
+};
+
+export default TodoForm;
